af85b2d2e4da8ba0f7050e0e2e686eae9707990e
sdcaulley <stephanie.caulley@gmail.com>
2019-11-07 11:29:56 -0600

    Updates

--- precache-manifest.a74e9860a92857a63a2e49e72f912932.js
+++ precache-manifest.a74e9860a92857a63a2e49e72f912932.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d86c6963d4c73df6485b729e7bf0cd68",
+    "revision": "3dcd13aabeabdb3a1f56b9b9abd4c40e",
     "url": "/hack-for-fun/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hack-for-fun/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "9d51d679872874ac136d",
-    "url": "/hack-for-fun/static/css/main.2066f8e5.chunk.css"
+    "revision": "0e379f0c093f941f777a",
+    "url": "/hack-for-fun/static/css/main.4f770622.chunk.css"
   },
   {
     "revision": "6db9ab4e0090cce1569a",
     "url": "/hack-for-fun/static/js/2.c7f75c47.chunk.js"
   },
   {
-    "revision": "9d51d679872874ac136d",
-    "url": "/hack-for-fun/static/js/main.86feb711.chunk.js"
+    "revision": "0e379f0c093f941f777a",
+    "url": "/hack-for-fun/static/js/main.4b8e9329.chunk.js"
   },
   {
     "revision": "2b2d852610c1dafaea01",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hack-for-fun/precache-manifest.5708abbb10b52f2f7046c8cf508f04bd.js"
+  "/hack-for-fun/precache-manifest.a74e9860a92857a63a2e49e72f912932.js"
 );
 
 self.addEventListener('message', (event) => {
